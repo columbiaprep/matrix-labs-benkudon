cd9edcb7ad54099af772b6d2610541431202bdde
Benjamin Kudon <benkudon@gmail.com>
2024-02-06 12:29:53 -0500

    done.

---
+++ Solutions.java
@@ -0,0 +1,11 @@
+public class Solutions {
+    public static int[][] scale(int[][] matrix, int scaleFactor) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                matrix[i][j] *= scaleFactor;
+            }
+        }
+        return matrix;
+    }
+
+}
