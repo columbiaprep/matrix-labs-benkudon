6b3e64b6d628c9bdf73eb28c3b35663ee3d4c17d
Benjamin Kudon <benkudon@gmail.com>
2024-02-09 14:59:29 -0500

    in class work on friday - not finished yet. next commit and push will be the finished code.

---
+++ Main.java
@@ -0,0 +1,5 @@
+public class Main {
+//print out new scaled matrix
+    //print out new mulitpled matrix
+    //print out both the new matrix of determinant
+}
--- Solutions.java
+++ Solutions.java
@@ -1,17 +1,33 @@
 public class Solutions {
-    public static int[][] scale(int[][] matrix, int scaleFactor) {
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                matrix[i][j] *= scaleFactor;
+    //make matrix/2d array of ints for 2x2 and 3x3 and make 2x2 for scaling algorithm to test it
+    int [][]matrixNader2x2 = new int[][]{{1,4,2}, {3,6,8}};
+    int [][]matrixBen3x3 = new int[][]{{3,9,1}, {5,7,3},{2,8,2}};
+    int [][]matrixForScale = new int[][]{{4,5,9}, {1,2,3}};
+    int scaleFactor = 2;
+
+
+    //multiply each value in the 2D array by that scaling factor and return array
+    public static int[][] scale(int[][] matrixForScale, int scaleFactor) {
+        for (int i = 0; i < matrixForScale.length; i++) {
+            for (int j = 0; j < matrixForScale[i].length; j++) {
+                matrixForScale[i][j] *= scaleFactor;
             }
         }
-        return matrix;//fasd
+        return matrixForScale;
     }
 
-    public static int findDeterminant(int[][] matrix) {
-        if (matrix.length != matrix[0].length) {
-            System.out.println("Invalid");
-            return 0;
-        }
+    //multiply matrixForScale and matrixNader2x2 (which are the same size)
+    public static int[][] multiplyMatrices(int[][] matrixForScale, int[][]matrixNader2x2) {
+    }
 
+    //find Determinant of the 2x2
+    public static int findDeterminant2x2(int[][] matrixNader2x2) {
+        }
+    //find Determinant of the 3x3
+    public static int findDeterminant3x3(int[][] matrixBen3x3) {
+
+        }
 }
+}
+
+
